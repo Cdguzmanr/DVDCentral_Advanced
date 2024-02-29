fa2c3fcc4751c5fcfded24c69f760edd7fc391e9
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-02-28 23:12:32 -0600

    Updated localhost address

--- CG.DVDCentral.ConsoleApp/Program.cs
+++ CG.DVDCentral.ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using CG.DVDCentral.ConsoleApp;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using CG.DVDCentral.ConsoleApp;
+
 
 internal class Program
 {
@@ -7,17 +11,58 @@ internal class Program
 
         Console.WriteLine("Which operation do you wish to perform?");
         Console.WriteLine("Connect to a channel (c)");
+        Console.WriteLine("Get the secret (g)");
         Console.WriteLine("Send a message to the channel (s)");
+
         Console.WriteLine("Exit (x)");
 
         string operation = Console.ReadLine();
         return operation;
     }
 
+    private async static void GetSecret()
+    {
+
+        const string secretName = "DVDCentral";
+        var keyVaultName = "kv-31590";
+        var kvUri = $"https://{keyVaultName}.vault.azure.net";
+
+        var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+        var secretValue = "SECRET_VALUE";
+
+        Console.Write($"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...");
+        await client.SetSecretAsync(secretName, secretValue);
+        Console.WriteLine(" done.");
+
+        Console.WriteLine("Forgetting your secret.");
+        secretValue = string.Empty;
+        Console.WriteLine($"Your secret is '{secretValue}'.");
+
+        Console.WriteLine($"Retrieving your secret from {keyVaultName}.");
+        var secret = await client.GetSecretAsync(secretName);
+        Console.WriteLine($"Your secret is '{secret.Value.Value}'.");
+
+        Console.Write($"Deleting your secret from {keyVaultName} ...");
+        //DeleteSecretOperation operation = await client.StartDeleteSecretAsync(secretName);
+        // You only need to wait for completion if you want to purge or recover the secret.
+        //await operation.WaitForCompletionAsync();
+        //Console.WriteLine(" done.");
+
+        //Console.Write($"Purging your secret from {keyVaultName} ...");
+        //await client.PurgeDeletedSecretAsync(secretName);
+        //Console.WriteLine(" done.");
+    }
+
+
     private static void Main(string[] args)
     {
         string user = "Carlos";
-        string hubAddress = "https://fvtcdp.azurewebsites.net/GameHub";
+        // Add new url
+        //string hubAddress = "https://fvtcdp.azurewebsites.net/GameHub";
+        //string hubAddress = "https://dvdcentralapi-300083002.azurewebsites.net/BingoHub";
+        string hubAddress = "https://localhost:7112/BingoHub";
+
         string operation = DrawMenu();
 
         var signalRConnection = new SignalRConnection(hubAddress);
@@ -28,6 +73,9 @@ internal class Program
             {
                 case "c":
                     signalRConnection.ConnectToChannel(user);
+                    break;
+                case "g":
+                    GetSecret();
                     break;
                 case "s":
                     break;
